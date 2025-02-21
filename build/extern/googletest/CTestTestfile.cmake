6378be18a56175671ea07edc7ff8c1e6bb4da2ba
Yoshinori Koyabu <ykoyabu@scc-kk.co.jp>
2025-02-21 11:12:15 +0900

    Update06

--- build/extern/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ykoyabu/my-project2/SampleApp/extern/googletest
-# Build directory: C:/Users/ykoyabu/my-project2/SampleApp/build/extern/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
